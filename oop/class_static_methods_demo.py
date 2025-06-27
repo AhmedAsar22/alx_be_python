5f016bae17c378dbb3159bfe23ffa189625a3141
Ahmed Asar <a7med3sar22@gmail.com>
2025-06-27 06:31:00 +0300

    add class_static_methods_demo

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,20 +1,11 @@
-class Duck:
-    def quack(self):
-        return "Duck quacks"
+class Calculator:
+    calculation_type = "Arithmetic Operations"
 
+    @staticmethod
+    def add(a, b):
+        return a + b
 
-class Person:
-    def quack(self):
-        return "Person imitates duck"
-
-
-# Polymorphic behavior using duck typing
-def make_sound(obj):
-    return obj.quack()
-
-
-duck_obj = Duck()
-person_obj = Person()
-
-print(make_sound(duck_obj))  # Output: "Duck quacks
-print(make_sound(person_obj))  # Output: "Person imitates duck"
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
